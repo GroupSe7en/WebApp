bddd27d68ac62566da0682e0e8aa936da3509af4
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2021-02-12 12:10:28 +0530

    StudentRequest model added

--- requester/models.py
+++ requester/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from users.models import CustomUser
 
-# Create your models here.
+#model for request
+class StudentRequest(models.Model):
+
+    TYPE_CHOICES = [
+    ('SD', 'Submission Deadline'),
+    ('T2', 'TYPE2'),
+    ('T3', 'TYPE3'),
+    ('OT', 'Other')
+    ]
+
+    requestType = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(CustomUser, related_name="student_requests", on_delete=models.CASCADE)
+    reciever = models.ForeignKey(CustomUser, related_name="lecturer_requests", on_delete=models.CASCADE)
+    accept_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
