1fb781d297f8132daaa88f53a0a78420374d0252
Shilpa-Perera <laksahaniperera@gmail.com>
2021-03-11 22:23:34 +0530

    s

--- requester/views.py
+++ requester/views.py
@@ -5,41 +5,6 @@ from .models import StudentRequest, Comment, CommentReply
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 import django_filters 
 
-# #home page of requester app
-# @login_required(login_url='')
-# def homeView(request):
-      
-#     if request.user.groups.filter(name='Lecturer').exists():
-#         context = {
-#           'StudentRequests': StudentRequest.objects.filter(reciever=request.user).all()
-#         }
-#         print(context)
-#         return render(request, 'requester/lecturerhome.html', context)#if user is a lecturer render lecturer home page
-#     elif request.user.groups.filter(name='Student').exists():
-#         context = {
-#           'StudentRequests': StudentRequest.objects.filter(author=request.user).all()
-#          }
-#         return render(request, 'requester/studenthome.html', context)#if user is a lecturer render lecturer home page
-
-#home page of requester app
-# class RequestListView(LoginRequiredMixin, ListView):
-
-#   context_object_name = 'StudentRequests'
-
-#   def get_queryset(self):
-#     if self.request.user.groups.filter(name='Lecturer').exists():
-#       queryset = StudentRequest.objects.filter(reciever=self.request.user).all().order_by('-date_posted')
-#     elif self.request.user.groups.filter(name='Student').exists():
-#         queryset = StudentRequest.objects.filter(author=self.request.user).all().order_by('-date_posted')
-#     return queryset
-
-#   def get_template_names(self):
-#     if self.request.user.groups.filter(name='Lecturer').exists():
-#       return ['requester/lecturerhome.html']
-#     elif self.request.user.groups.filter(name='Student').exists():
-#       return ['requester/studenthome.html']
-
-#######################################################################################################################################
 
 class StudentRequestFilterset(django_filters.FilterSet):
     class Meta:
